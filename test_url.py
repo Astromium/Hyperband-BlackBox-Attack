b525d4b707e32cead81893949d6d46ddeb324fac
Astromium <interstellarspace2000@gmail.com>
2023-04-05 14:54:11 +0200

    preparing to pull

--- test_url.py
+++ test_url.py
@@ -66,15 +66,15 @@ if __name__ == '__main__':
     # Parameters for Hyperband
     dimensions = X_test.shape[1]
     BATCH_SIZE = 100
-    eps = 0.05
-    downsample = 2
+    eps = 0.2
+    downsample = 3
     sampler = Sampler()
     distance = 'l2'
     classifier_path = './ressources/model_url.h5'
     success_rates_l2 = []
     exec_times_l2 = []
 
-    R_values = [256]
+    R_values = [81]
     history_dict = dict()
     '''
     for eps in perturbations:
